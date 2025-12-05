43c1c364f26f9ddaeed716b288898cdf04a7bdfe
NikolozGeladze <nikolozgeladze7@gmail.com>
2025-12-05 12:57:27 +0400

    Add files via upload

---
+++ BankingSystem/Bank/BankingSystem.cs
@@ -0,0 +1,58 @@
+﻿namespace ATM.Bank
+{
+    public class BankingSystem
+    {
+        /// <summary>
+        /// Decimal field to store the current balance.
+        /// </summary>
+        private decimal _balance;
+
+        public BankingSystem(decimal initialBalance)
+        {
+            _balance = initialBalance;
+        }
+
+        /// <summary>
+        /// Shows the current balance to the console.
+        /// </summary>
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Balance: {_balance}");
+        }
+
+        /// <summary>
+        /// Withdraws money from the balance if sufficient funds are available.
+        /// </summary>
+        public void WithdrawMoney(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("Amount must be positive.");
+            }
+            if (amount > _balance)
+            {
+                throw new Exception("Not enough balance.");
+            }
+
+            _balance -= amount;
+        }
+
+        /// <summary>
+        /// Adds money to the balance.
+        /// </summary>
+        public void AddMoney(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("Amount must be positive.");
+            }
+            _balance += amount;
+        }
+
+        /// <summary>
+        /// Gets the current balance.
+        /// </summary>
+        public decimal GetBalance() => _balance;
+    }
+}
+
---
+++ BankingSystem/Bank/KeyListener.cs
@@ -0,0 +1,55 @@
+﻿internal class KeyListener
+{
+    /// <summary>
+    /// Event triggered to check the balance.
+    /// </summary>
+    public event Action OnBalanceCheck = null!;
+    public event Action<decimal> OnWithdraw = null!;
+    public event Action<decimal> OnAddMoney = null!;
+
+    /// <summary>
+    /// Starts listening for key inputs to perform banking operations.
+    /// </summary>
+    public void StartListening()
+    {
+        while (true)
+        {
+            ConsoleKeyInfo key = InsertKey();
+
+            if (key.Key == ConsoleKey.Escape)
+                break;
+
+            switch (key.Key)
+            {
+                case ConsoleKey.W:
+                    Console.Write("Enter amount: ");
+                    OnWithdraw?.Invoke(Convert.ToDecimal(Console.ReadLine()));
+                    break;
+
+                case ConsoleKey.A:
+                    Console.Write("Enter amount: ");
+                    OnAddMoney?.Invoke(Convert.ToDecimal(Console.ReadLine()));
+                    break;
+
+                case ConsoleKey.B:
+                    OnBalanceCheck?.Invoke();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid key!");
+                    break;
+            }
+        }
+    }
+    /// <summary>
+    /// Inserts a key and displays the menu options.
+    /// </summary>
+    private static ConsoleKeyInfo InsertKey()
+    {
+        Console.WriteLine("\nA - Add money");
+        Console.WriteLine("B - Show balance");
+        Console.WriteLine("W - Withdraw money");
+        Console.WriteLine("ESC - Exit");
+        return Console.ReadKey(true);
+    }
+}
